97f8cf70ea3a1781e7a5654b4546fc6ef3b35085
Anabtawi-group <malnsour@anabtawisweets.com>
2026-01-21 11:46:38 +0300

    Pledge: load pledge category into POS

--- addons/pos_double_receipt_pledge/models/pos_session.py
+++ addons/pos_double_receipt_pledge/models/pos_session.py
@@ -1,1 +1,11 @@
+from odoo import models
 
+class PosSession(models.Model):
+    _inherit = "pos.session"
+
+    def _loader_params_pos_config(self):
+        params = super()._loader_params_pos_config()
+        fields = params["search_params"].setdefault("fields", [])
+        if "pledge_pos_categ_id" not in fields:
+            fields.append("pledge_pos_categ_id")
+        return params
