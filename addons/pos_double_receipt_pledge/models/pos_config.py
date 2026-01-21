864f698d7976d9951de98a87a3e3df228d227c88
Anabtawi-group <malnsour@anabtawisweets.com>
2026-01-21 11:43:04 +0300

    Pledge: add POS config field

--- addons/pos_double_receipt_pledge/models/pos_config.py
+++ addons/pos_double_receipt_pledge/models/pos_config.py
@@ -1,1 +1,10 @@
+from odoo import fields, models
 
+class PosConfig(models.Model):
+    _inherit = "pos.config"
+
+    pledge_pos_categ_id = fields.Many2one(
+        "pos.category",
+        string="Pledge POS Category",
+        help="If an order contains products in this POS Category, POS prints a second pledge-only receipt and enforces customer mobile."
+    )
